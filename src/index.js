2cda38b8fac05ed69385a47de920f16d9233d54f
Patrick Oliveira <patrick.oliveira@veolink.com.br>
2023-06-21 10:52:19 -0300

    correção caminho

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './galeria.css';
-import Principia from './teste';
+import Principia from './Principia';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
