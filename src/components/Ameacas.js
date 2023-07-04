03b96f62013893e9bd205d4b64cfe9eff8554beb
Patrick Oliveira <patrick.oliveira@veolink.com.br>
2023-07-04 14:41:44 -0300

    geração dinamica bando

--- src/components/Ameacas.js
+++ src/components/Ameacas.js
@@ -4,5 +4,24 @@ export const Ameacas = {
 };
 
 function get() {
-    return ameacas;
+    //return ameacas;
+    let ameacasRetorno = [];
+    ameacasRetorno.push(ameacas[0]);
+    for (let index = 1; index < ameacas.length; index++) {
+        const element = ameacas[index];
+        if (element.quantidade && element.quantidade > 1) {
+            for (let j = 0; j < element.quantidade; j++) {
+                let x = {...element, nome:  element.nome + " " + parseInt(j+1)};
+                ameacasRetorno.push(x);
+            }
+        }else {
+            let x = {...element, nome:  element.nome};
+            ameacasRetorno.push(x);
+        }
+    }
+    debugger;
+   for (let index = 0; index < ameacasRetorno.length; index++) {
+    ameacasRetorno[index].index = index;
+   }
+    return ameacasRetorno;
 }
